ed3744aed8a229bfc49d6e2fb8ccff0c368efe4d
mudit-51 <mudit.7.gupta+github@gmail.com>
2024-06-08 13:58:19 +0530

    added distance calculation caching

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from itertools import accumulate
 from time import sleep
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
@@ -6,8 +7,11 @@ from collections import deque
 import osmnx
 import osmnx.convert
 import osmnx.distance
+import osmnx.routing
+from geopandas import GeoDataFrame
 
 from threading import Thread
+
 from threads import location_thread
 
 from db.supabase import supabase
@@ -22,14 +26,14 @@ async def lifespan(app: FastAPI):
     print('Application online!')
 
     G = osmnx.graph_from_place(
-        'Shiv Nadar University, India', custom_filter="['highway'~'service']"
+        'Shiv Nadar University, India', custom_filter="['highway'~'service|pedestrian']"
     )
     G.remove_nodes_from([4227700806, 4227700802, 4314526562, 4227700788, 5287350292])
 
-    app.state.GDF = osmnx.convert.graph_to_gdfs(G)
     app.state.G = G
-
-    all_shuttle_routes: dict[int, list] = {}
+    GDF = osmnx.convert.graph_to_gdfs(G)
+    
+    stop_distance: dict[int, list] = {}
     stop_locations: dict[int, list[tuple[float, float]]] = {}
 
     route_ids = supabase.table('Route').select('*').execute().data
@@ -37,18 +41,40 @@ async def lifespan(app: FastAPI):
         shuttle_route = (
             supabase.table('Stops').select('*').eq('route_id', i['id']).execute().data
         )
-        all_shuttle_routes.update({i['id']: shuttle_route})
-
+        dist = []
         temp = []
         for j in shuttle_route:
             temp.append((j['lat'], j['long']))
-        print(temp)
+        
+        for j in range(1, len(temp)):
+            start = temp[j-1]
+            end = temp[j]
+            start_node = osmnx.distance.nearest_nodes(G, start[1], start[0])
+            end_node = osmnx.distance.nearest_nodes(G, end[1], end[0])
+            path = osmnx.routing.shortest_path(G, start_node, end_node)
+
+            total_distance = 0
+            if path and len(path)>1:
+                temp_gfd: GeoDataFrame = osmnx.routing.route_to_gdf(G, path)
+                route_length: list = list(accumulate(temp_gfd['length']))
+                total_distance += route_length[-1]
+
+            start_node_loc = GDF[0].loc[start_node]
+            end_node_loc =  GDF[0].loc[end_node]
+
+            total_distance += osmnx.distance.great_circle(start[0], start[1], start_node_loc.y, start_node_loc.x)
+            total_distance += osmnx.distance.great_circle(end[0], end[1], end_node_loc.y, end_node_loc.x)
+            
+            dist.append(total_distance)
+
+        stop_distance.update({i['id']: dist})
         stop_locations.update({i['id']: temp})
 
-    app.state.stop_locations = stop_locations
-    app.state.all_shuttle_routes = all_shuttle_routes
 
-    t = Thread(target=location_thread.test, args=[app], daemon=True).start()
+    app.state.stop_distance = stop_distance
+    app.state.stop_locations = stop_locations
+
+    Thread(target=location_thread.test, args=[app], daemon=True).start()
 
     # osmnx.plot_graph(app.state.G)
     yield
